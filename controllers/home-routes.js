753e807abfaa4b8f29cc6d9481d8565841182706
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-21 16:13:23 -0700

    create login and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,9 @@ const sequelize = require("../config/connection.js");
 const { Post, User, Comment } = require("../models");
 
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id',
@@ -18,9 +20,9 @@ router.get("/", (req, res) => {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
                 include: {
-                model: User,
-                attributes: ['username']
-            }
+                    model: User,
+                    attributes: ['username']
+                }
             },
             {
                 model: User,
@@ -29,15 +31,23 @@ router.get("/", (req, res) => {
         ]
     })
         .then(dbPostData => {
-        console.log(dbPostData[0]);
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            // pass a single post object into the homepage template
-            res.render('homepage',{ posts });
+    
+            res.render('homepage', { posts });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+  
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+  
+    res.render('login');
 });
 
 module.exports = router;
